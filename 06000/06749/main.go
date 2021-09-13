ef40155510444c0ab7f7f1cca74c2d837ce6f2aa
kangdonguk <kangdongouk@gmail.com>
2021-09-13 11:52:14 +0900

    06749 solve

---
+++ 06000/06749/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	rd *bufio.Reader
+	wr *bufio.Writer
+)
+
+func init() {
+	rd = bufio.NewReader(os.Stdin)
+	wr = bufio.NewWriter(os.Stdout)
+}
+
+func main() {
+	defer wr.Flush()
+
+	var a, b int
+	fmt.Fscan(rd, &a, &b)
+
+	fmt.Fprintf(wr, "%d", b + b - a);
+}
