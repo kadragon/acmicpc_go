45f5f2b6e866078dc16f045daad1fe07ee7a5192
kangdonguk <kangdongouk@gmail.com>
2021-12-29 10:58:21 +0900

    23803 solve

---
+++ 23000/23803/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	sc *bufio.Scanner
+	wr *bufio.Writer
+)
+
+func main() {
+	sc = bufio.NewScanner(os.Stdin)
+	wr = bufio.NewWriter(os.Stdout)
+	defer wr.Flush()
+
+	n := nextInt()
+
+	d := strings.Repeat("@", n*5)
+
+	for i := 0; i < n*5; i++ {
+		if i < n*4  {
+			fmt.Fprintf(wr, "%s\n", d[:n])
+		} else {
+			fmt.Fprintf(wr, "%s\n", d)
+		}
+	}
+}
+
+func nextInt() int {
+	sc.Scan()
+	v, _ := strconv.Atoi(sc.Text())
+
+	return v
+}
