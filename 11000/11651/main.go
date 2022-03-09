34c9c2a7952bcf792cf360f7d73ceeb6a5696f49
kangdonguk <kangdongouk@gmail.com>
2022-03-09 21:27:16 +0900

    11651 solve

---
+++ 11000/11651/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var (
+	rd *bufio.Reader
+	wr *bufio.Writer
+)
+
+func init() {
+	rd = bufio.NewReader(os.Stdin)
+	wr = bufio.NewWriter(os.Stdout)
+}
+
+func main() {
+	defer wr.Flush()
+
+	var n int
+	fmt.Fscan(rd, &n)
+
+	d := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(rd, &d[i][0], &d[i][1])
+	}
+
+	sort.Slice(d, func(i, j int) bool {
+		if d[i][1] != d[j][1] {
+			return d[i][1] < d[j][1]
+		}
+		return d[i][0] < d[j][0]
+	})
+
+	for _, v := range d {
+		fmt.Fprintf(wr, "%d %d\n", v[0], v[1])
+	}
+}
